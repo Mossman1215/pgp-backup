31d856975a6ff0baab5917ef2a8537f63fd384f0
Moss Cantwell <mossman2142@gmail.com>
2016-02-09 08:11:36 +1300

    bug fix - fixed /./ issue, using apiv2, can now upload a nested directory

--- encryptedUploader.py
+++ encryptedUploader.py
@@ -14,9 +14,12 @@ class encryptedUploader:
        self.oauth_folder = os.path.normpath(sys.argv[3])
        self.encrypted = "/tmp"
        self._create_drive()
+       if len(sys.argv) > 4:
+           self.debug = bool(sys.argv[4])
+       else:
+           self.debug = False
 
     def run(self):
-        print "test"
         self._encryptDir(self.source,self.remote)
 
 #need to create new drive folders of mime type google.folder thing
@@ -25,27 +28,40 @@ class encryptedUploader:
         gpg = gnupg.GPG()
         key_data = open('backup_pub.gpg').read()
         import_result = gpg.import_keys(key_data)
-        print(import_result.results)
+        #always import keys so that it is in memory/keyring
         fingerprint = '0402C9ADB9626A45700A39F0663890816CDA1490'
         parent_folder_id =[]
         parent_folder_id.insert(0,self._get_folder_id(remote))
-        folder_response = self.drive_service.files().list(q="title='%s' and mimeType contains 'application/vnd.google-apps.folder' and trashed=false" % remote).execute()
+        folder_response = self.drive_service.files().list(q="mimeType contains 'application/vnd.google-apps.folder' and trashed=false").execute()
         folders = folder_response['items']
-        files_response = self.drive_service.files().list(q="title='%s' and mimeType contains 'video/avi' and trashed=false" % remote).execute() 
+        if len(folders) > 0:
+            folders = folders[0]['title']
+        else:
+            folders = []
+        files_response = self.drive_service.files().list(q="mimeType contains 'video/avi' and trashed=false").execute() 
         files_remote = files_response['items']
-        print "folders " + str(folders) +" files " +str(files_remote)
+        if len(files_remote) > 0:
+            files_remote = files_remote[0]['title']
+        else:
+            files_remote = []
+        print "folders " + str(folders) + " files " + str(files_remote)
+        if self.debug == True:
+            exit()
         for root, dirs, files in os.walk(source, topdown=True):           
             print root
             subdir = os.path.relpath(root,self.source)
             remote_path = os.path.join(remote,subdir)
             print remote_path
-            if not remote_path in folders:
+            if not subdir in folders:
                 print 'folder is not in remote'
-                current_id = self.make_folder(root,parent_folder_id[0])
-                parent_folder_id.insert(0,current_id)
+                if not root.endswith('.'):
+                    current_id = self.make_folder(root,parent_folder_id[0])
+                    parent_folder_id.insert(0,current_id)
             for name in files:
                 file_path = os.path.join(root, name)
                 subdir = os.path.relpath(root,self.source)
+                if(subdir.find('./')!=-1):
+                    subdir = subdir[2:]
                 newpath = os.path.join(self.encrypted,subdir)
                 encrypted_path = os.path.join(newpath,name)
                 encrypted_path += '.gpg'
@@ -57,8 +73,10 @@ class encryptedUploader:
                     with open(file_path, 'rb') as f:
                         if(not name+".gpg" in files_remote):
                             print "file not in remote "+remote_path+".gpg"
+                        if(not os.path.exists(newpath)):
+                            os.makedirs(newpath)#make the folder in /tmp if it does not exist
                         messsage = str(gpg.encrypt(f.read(),fingerprint,output=encrypted_path))
-                        self.upload_file(encrypted_path+,parent_folder_id[0])
+                        self.upload_file(encrypted_path,parent_folder_id[0])
             if(len(parent_folder_id)>0):
                 parent_folder_id.pop();
     def _create_drive(self):
