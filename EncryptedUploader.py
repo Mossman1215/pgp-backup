f3d77faa4dfe9ad76a87aac1d92fb61e02be1043
Moss Cantwell <mossman2142@gmail.com>
2016-06-28 17:34:45 +1200

    Rebuilt encrypted uploader by merging upload_dir and encrypt modules together

--- EncryptedUploader.py
+++ EncryptedUploader.py
@@ -5,73 +5,54 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client.file import Storage
 from googleapiclient.http import MediaFileUpload
+from encrypt import encrypt
 import logging
 
-class encryptedUploader:
+class EncryptedUploader:
     def __init__(self):
        self.source = os.path.normpath(sys.argv[1])
        self.remote = os.path.normpath(sys.argv[2])
        self.oauth_folder = os.path.normpath(sys.argv[3])
-       self.encrypted = "/tmp"
        self._create_drive()
-       if len(sys.argv) > 4:
-           self.debug = bool(sys.argv[4])
-       else:
-           self.debug = False
-
+       self.mapping = dict()
+       self.encrypter = encrypt()
     def run(self):
-        self._encryptDir(self.source,self.remote)
-
-#need to create new drive folders of mime type google.folder thing
-#parenting correctly with their ID number
-    def encryptDir(self,source,remote):
-        gpg = gnupg.GPG()
-        key_data = open('backup_pub.gpg').read()
-        import_result = gpg.import_keys(key_data)
-        #always import keys so that it is in memory/keyring
-        fingerprint = '0402C9ADB9626A45700A39F0663890816CDA1490'
-        for root, dirs, files in os.walk(source, topdown=True):           
-            print root
-            subdir = os.path.relpath(root,self.source)
-            if not root.endswith('.'):
-                current_id = self.make_folder(root,parent_folder_id[0])
-                parent_folder_id.insert(0,current_id)
-            for name in files:
-                file_path = os.path.join(root, name)
-                subdir = os.path.relpath(root,self.source)
-                if(subdir.find('./')!=-1):
-                    subdir = subdir[2:]
-                newpath = os.path.join(self.encrypted,subdir)
-                encrypted_path = os.path.join(newpath,name)
-                encrypted_path += '.gpg'
-                statinfo = os.stat(file_path)
-                        
-                if statinfo.st_size<100000000 and name != '.':
-                    with open(file_path, 'rb') as f:
-                        if(not os.path.exists(newpath)):
-                            os.makedirs(newpath)#make the folder in /tmp if it does not exist
-                        messsage = str(gpg.encrypt(f.read(),fingerprint,output=encrypted_path))
-                        #self.upload_file(encrypted_path,parent_folder_id[0])
-            #pop parent folder?
-    def upload_dir(self,source,remote):
-        parent_folder_id =[]
-        parent_folder_id.insert(0,self._get_folder_id(remote)
-        for root,subdirs,files in os.walk(source, topdown=true)
-            query = self._get_folder_id(root)
-            if query['title'] == os.path.basename(root):#root exists in remote
-                update_folder(root,files)
-            else:
-                parent_folder_id.insert(0,self.upload_new(root,files))
-
-    def update_folder(self,root,files,pid):
-        #check for folder
-        return 0
-    def upload_new(self,root,files,pid):
-        fid = make_folder(os.path.basename(root),pid)
-        for items in files
-            upload_file(os.path.basename(item),fid)
-        return fid
-        
+        self.upload_dir()
+    
+    def getParentID(self,filename):
+        #search the dictionary for the filename
+        if(filename in self.mapping):
+            return self.mapping[filename]
+        else:
+            return -1
+            
+    def upload_dir(self):
+        print(self.number_of_operations())
+        for root,subdirs,files in os.walk(self.source, topdown=True):
+            title = os.path.basename(root)
+            pID = self.getParentID(title)
+            if(pID == -1):
+                pID = self.get_folder_id(self.remote)#add error code if folder does not exist make it! or crash with helpfull message
+                self.mapping[title] = pID
+            identifier = self.make_folder(title,pID)
+            for subdir in subdirs:
+                subName = os.path.basename(subdir)
+                self.mapping[subName] = identifier
+            for fi in files:
+                file_path = os.path.join(root,fi)
+                print(file_path)
+                #encrypt
+                temporary_name = encrypter.encrypt(file_path)
+                self.upload_file(temporary_name,identifier)
+                os.remove(temporary_name)
+    
+    def number_of_operations(self):
+        count = 0
+        for root,subdirs,files in os.walk(self.source, topdown=True):
+            count+=1
+            count= count + len(files)
+        return count
+    
     def _create_drive(self):
         """Create a Drive service."""
         auth_required = True
@@ -95,7 +76,7 @@ class encryptedUploader:
         if auth_required:
             flow = client.flow_from_clientsecrets(
                 self.oauth_folder+'/client_secrets.json',
-                scope='https://www.googleapis.com/drive',
+                scope='https://www.googleapis.com/auth/drive',
                 redirect_uri='urn:ietf:wg:oauth:2.0:oob')
             auth_uri = flow.step1_get_authorize_url()
 
@@ -110,7 +91,7 @@ class encryptedUploader:
         http_auth = credentials.authorize(httplib2.Http())
         self.drive_service = discovery.build('drive', 'v2', http_auth)
             
-    def _get_folder_id(self, folder_name):
+    def get_folder_id(self, folder_name):
         """Find and return the id of the folder given the title."""
         files = self.drive_service.files().list(q="title='%s' and mimeType contains 'application/vnd.google-apps.folder' and trashed=false" % folder_name).execute()
         if len(files['items']) == 1:
@@ -119,9 +100,9 @@ class encryptedUploader:
         else:
             return "folder not found"
 
-    def make_folder(self,path,parent_id):
+    def make_folder(self,title,parent_id):
         file_metadata = {
-            'title' : str(os.path.basename(path)),
+            'title' : title,
             'parents' : [{'id':parent_id}],
             'mimeType' : 'application/vnd.google-apps.folder'
         }
@@ -135,7 +116,6 @@ class encryptedUploader:
         response = self.drive_service.files().insert(media_body=media, body={'title':os.path.basename(file_path), 'parents':[{u'id': parent}]}).execute()
         #print response
         video_link = response['alternateLink']
-        
         #if self.delete_after_upload:
             #os.remove(file_path)    
 
