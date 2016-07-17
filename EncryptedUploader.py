df70e0b977ef07be097b9496b92a5dc01993faf4
Moss Cantwell <mossman2142@gmail.com>
2016-07-17 21:18:58 +1200

    change type of error to catch

--- EncryptedUploader.py
+++ EncryptedUploader.py
@@ -48,8 +48,8 @@ class EncryptedUploader:
                 temporary_name = self.encrypter.encrypt(file_path)
                 try:
                     self.upload_file(temporary_name,identifier)
-                except IOError as arg:
-                    print("file io error" + str(arg))
+                except TypeError as arg:
+                    print("file type error: " + str(arg))
                 time.sleep(1)
                 os.remove(temporary_name)
     
