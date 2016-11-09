5f4cb828c66142789779555dea0516edc890ba4b
Moss Cantwell <mossman2142@gmail.com>
2016-11-10 08:20:14 +1300

    changed encrypt_dir to use encrypt module and just iterate over desired folder

--- encrypt_dir.py
+++ encrypt_dir.py
@@ -2,35 +2,21 @@ import sys
 import os
 import gnupg
 import time
-if(len(sys.argv)<2):
-	print "not enough arguments"
-        print "must have <source> directory"
-	exit()
-source = os.path.normpath(sys.argv[1])
-gpg = gnupg.GPG()
-key_data = open('backup_pub.gpg').read()
-import_result = gpg.import_keys(key_data)
-print(import_result.results)
-fingerprint = '0402C9ADB9626A45700A39F0663890816CDA1490'
-encrypted = '/tmp'
+import encrypt
+
+config = ConfigParser.SafeConfigParser()
+config.read('options.conf')
+encrypted = config.get('folder','temp',1)
+crypto = encrypt()
 
 for root, dirs, files in os.walk(source, topdown=True):           
     for name in files:
         file_path = os.path.join(root, name)
-        print file_path
-        newpath = os.path.relpath(root,source)
+        newpath = os.path.relpath(root,filepath)
         newpath = os.path.join(encrypted,newpath)
-        encrypted_path = os.path.join(newpath,name)
         if(not os.path.exists(newpath)):
             os.makedirs(newpath)
-            print 'built path'
-        statinfo = os.stat(file_path)
-        if statinfo.st_size<100000000:
-                with open(file_path, 'rb') as f:
-                        message = str(gpg.encrypt(f.read(),fingerprint,output=encrypted_path+'.gpg'))
-                f.close()
-        print 'file done'
+        crypto.encrypt(file_path)
         
 print 'All Done!'
 #print subprocess.call(command,stdout=result,stderr=resulterror)
-print 'synched'
